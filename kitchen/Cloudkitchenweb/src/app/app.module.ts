76b40d4455404992b1ce1ddf65fa98c24d6ee7b8
sanket <sanketmaradiya01@gmail.com>
2022-10-31 19:08:54 +0530

    commit

--- kitchen/Cloudkitchenweb/src/app/app.module.ts
+++ kitchen/Cloudkitchenweb/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './masterpage/header/header.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
